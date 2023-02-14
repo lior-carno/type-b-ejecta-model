d27051978b435e3c034c8981728f558cb72135cc
lior-carno <124742152+lior-carno@users.noreply.github.com>
2023-02-14 01:36:40 +0000

    Delete constants directory

--- constants/simulation_constants.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-dR = 10 ** -8  # Radial grid spacing
-dt = 10 ** -4  # Time step
-SIMULATION_LENGTH = 0.02  # Final time
-PARTICLE_RADIUS = 384.5 * (10 ** -6)  # Particle radius
-
-POSITION_GRID = np.arange(10 ** -20, PARTICLE_RADIUS, dR)
-TIME_GRID = np.arange(0, SIMULATION_LENGTH, dt)
